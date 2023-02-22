3a359d0037c6b94868f892c7622b115c74996aa3
courtney.hurt <courtney.hurt@checkr.com>
2023-02-21 21:10:00 -0800

    updates

--- app/worker.py
+++ app/worker.py
@@ -2,12 +2,14 @@
 import os
 import redis
 import time
+from .services import SearchService
 from rq import Worker, Queue, Connection
 from datetime import datetime, timedelta
 
-# Define a function that performs a task
+
 def my_task():
     SearchService().run()
+
 
 # Set up the Redis connection and queue
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
@@ -15,11 +17,14 @@ conn = redis.from_url(redis_url)
 q = Queue(connection=conn)
 
 # Define a function to start the worker
+
+
 def start_worker():
     listen = ['high', 'default', 'low']
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
+
 
 # Define the time interval between each job run (in seconds)
 interval = 3600
